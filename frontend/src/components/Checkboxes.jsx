2c84449ad92754daac41ed243bcafc90c717d90f
SlackerEva <lesiv_anna@mail.ru>
2022-05-06 15:42:02 +0300

    added logic for check cb

--- frontend/src/components/Checkboxes.jsx
+++ frontend/src/components/Checkboxes.jsx
@@ -1,33 +1,17 @@
-import React, {useState, useEffect} from 'react';
+//import React, {useState, useEffect} from 'react';
 import { FormControlLabel, Checkbox, FormGroup } from '@mui/material';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkCb } from '../store/codeSlice';
 
 function Checboxes() {
 
+  const dispatch = useDispatch();
   const codes = useSelector(state => state.codes.codes);
-
-/*  useEffect(() => {
-    setIsCheck(countries.map((country) => country['code']));
-    if (!checked) {
-      setIsCheck([]);
-    }
-  }, [checked, countries]);
-
-  function handleClick(event) {
-    const { id, checked } = event.target;
-    if (checked) {
-      setIsCheck([...isCheck, id]);
-      console.log(isCheck);
-    } else {
-      setIsCheck(isCheck.filter(item => item !== id));
-      console.log(isCheck);
-    }
-  }*/
   
   return (
     <FormGroup>
-      {codes.map((country) => {
-        return <FormControlLabel key={country['code']} control={<Checkbox  id={country['code']} />} label={country['country']} />
+      {codes.map((code) => {
+        return <FormControlLabel key={code['code']} control={<Checkbox key={code['code']} checked={code.check} onChange={(e) => dispatch(checkCb({code}))}/>} label={code['country']} />
       })}
     </FormGroup>
   );
--- frontend/src/store/codeSlice.js
+++ frontend/src/store/codeSlice.js
@@ -8,10 +8,14 @@ const codeSlice = createSlice({
     reducers: {
       addCodes(state, action) {
         //state.codes.push(action.payload.codes);
-        state.codes = [...action.payload.codes]
+        state.codes = [...action.payload.codes];
+      },
+      checkCb(state, action) {
+        const cb = state.codes.find(code => code.code === action.payload.code.code);
+        cb.check = !cb.check;
       }
     }
   });
 
-export const { addCodes } = codeSlice.actions;
+export const { addCodes, checkCb } = codeSlice.actions;
 export default codeSlice.reducer;
